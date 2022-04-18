0dba09faa2a95f1bb647abd90a2cc6828647cb41
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2022-04-18 23:45:51 +0600

    [ADD] note-detail & markdown

--- client/src/pages/NoteDetails.js
+++ client/src/pages/NoteDetails.js
@@ -1,11 +1,32 @@
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+`;
 
 export default function NoteDetails() {
 	const { noteId } = useParams();
 
 	return (
-		<section>
-			<div className="">note id = {noteId}</div>
+		<section className="">
+			<article className="w-full md:w-10/12 mx-auto prose lg:prose-xl prose-img:rounded-lg prose-headings:text-pink-600 prose-heading:font-semibold prose-a:text-pink-600  bg-slate-200 p-4 rounded">
+				<ReactMarkdown
+					children={markdown}
+					remarkPlugins={[remarkGfm]}
+				/>
+			</article>
 		</section>
 	);
 }
